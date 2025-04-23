176dc79878b9bcd15673764b30e30b3b8d02449f
surz <36134492+surz90@users.noreply.github.com>
2025-04-23 11:25:27 +0700

    add floor

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import {} from '@dcl/sdk/math'
 import { engine } from '@dcl/sdk/ecs'
 import { setupUi } from './ui'
 import { initDispensers } from './modules/dispenser/dispensers'
+import { setupEnvironment } from './environment'
 
 export function main() {
     setupUi()
-
+    setupEnvironment()
     initDispensers()
 }
